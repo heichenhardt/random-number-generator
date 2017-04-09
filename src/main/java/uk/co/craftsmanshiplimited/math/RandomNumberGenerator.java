b60d1004e4fff2897e41345c2d25a4f8a97142b8
Henrik Eichenhardt <henrik.eichenhardt@gmail.com>
2017-04-09 10:42:28 +0100

    Initial commit

---
+++ src/main/java/uk/co/craftsmanshiplimited/math/RandomNumberGenerator.java
@@ -0,0 +1,43 @@
+package uk.co.craftsmanshiplimited.math;
+
+import org.apache.commons.math3.distribution.EnumeratedIntegerDistribution;
+import org.apache.commons.math3.exception.DimensionMismatchException;
+import org.apache.commons.math3.exception.MathArithmeticException;
+import org.apache.commons.math3.exception.NotFiniteNumberException;
+import org.apache.commons.math3.exception.NotPositiveException;
+import org.apache.commons.math3.exception.NotANumberException;
+
+/**
+ * Created by Henrik on 09/04/2017.
+ */
+public class RandomNumberGenerator {
+
+    private final EnumeratedIntegerDistribution distribution;
+
+    public RandomNumberGenerator(
+            final int[] randomNumbers,
+            final double[] probabilities) {
+
+        try {
+            this.distribution =
+                    new EnumeratedIntegerDistribution(
+                        randomNumbers, probabilities);
+
+        } catch (final DimensionMismatchException
+                | NotPositiveException
+                | MathArithmeticException
+                | NotFiniteNumberException
+                | NotANumberException e) {
+
+            //Wrap the implementation specific exceptions.
+            final String message =
+                    e.getClass().getSimpleName()
+                            .replace("Exception", "") + " " + e.getMessage();
+            throw new IllegalArgumentException(message);
+        }
+    }
+
+    public final int nextNum() {
+        return this.distribution.sample();
+    }
+}
