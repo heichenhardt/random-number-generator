f9158f442e90c840b6f68d819d01e3cc96098c4f
heichenhardt <henrik.eichenhardt@gmail.com>
2017-04-19 19:07:53 +0100

    Review comments.

--- src/main/java/uk/co/craftsmanshiplimited/math/RandomNumberGeneratorCustom.java
+++ src/main/java/uk/co/craftsmanshiplimited/math/RandomNumberGeneratorCustom.java
@@ -1,7 +1,5 @@
 package uk.co.craftsmanshiplimited.math;
 
-import org.apache.commons.math3.distribution.EnumeratedIntegerDistribution;
-import org.apache.commons.math3.exception.*;
 import org.apache.commons.math3.util.MathArrays;
 
 import java.util.Arrays;
@@ -23,30 +21,33 @@ public class RandomNumberGeneratorCustom implements Randomable {
         this.random = new Random();
         this.randomNumbers = randomNumbers;
 
-        if(randomNumbers.length != probabilities.length) {
+        if (randomNumbers.length != probabilities.length) {
             throw new IllegalArgumentException(
-                    "DimensionMismatch " + randomNumbers.length + "!=" + probabilities.length);
+                    "DimensionMismatch "
+                            + randomNumbers.length
+                            + "!="
+                            + probabilities.length);
         }
-        if(Arrays.stream(probabilities).anyMatch(x->x < 0)) {
+        if (Arrays.stream(probabilities).anyMatch(x->x < 0)) {
             throw new IllegalArgumentException(
-                    "NotPositive - Probabilities must be 0 or positive" );
+                    "NotPositive - Probabilities must be 0 or positive");
         }
 
-        if(Arrays.stream(probabilities).anyMatch(x-> Double.isInfinite(x))) {
+        if (Arrays.stream(probabilities).anyMatch(x-> Double.isInfinite(x))) {
             throw new IllegalArgumentException(
-                    "NotFiniteNumber - Probabilities must be finit" );
+                    "NotFiniteNumber - Probabilities must be finit");
         }
-        if(Arrays.stream(probabilities).anyMatch(x-> Double.isNaN(x))) {
+        if (Arrays.stream(probabilities).anyMatch(x-> Double.isNaN(x))) {
             throw new IllegalArgumentException(
-                    "NotANumber - Probabilities must not be NaN" );
+                    "NotANumber - Probabilities must not be NaN");
         }
 
         final double[] normalizedProbabilities =
                 MathArrays.normalizeArray(probabilities, 1.0);
         this.probabilities = new double[normalizedProbabilities.length];
-        double sum=0;
+        double sum = 0;
         //Calculate precalc sums
-        for(int index=0; index < normalizedProbabilities.length; index++) {
+        for (int index = 0; index < normalizedProbabilities.length; index++) {
             sum += normalizedProbabilities[index];
             this.probabilities[index] = sum;
         }
@@ -55,11 +56,11 @@ public class RandomNumberGeneratorCustom implements Randomable {
     @Override
     public final int nextNum() {
         double randomNumber = this.random.nextDouble();
-        for(int index=0; index<this.probabilities.length; index++) {
-            if(randomNumber < this.probabilities[index]) {
-                return this.randomNumbers[index];
-            }
-        }
-        return this.randomNumbers[this.randomNumbers.length - 1];
+        return this.randomNumbers[getIndex(randomNumber)];
+    }
+
+    private int getIndex(final double randomNumber) {
+        int index = Arrays.binarySearch(this.probabilities, randomNumber);
+        return index >= 0 ? index : Math.abs(index) - 1;
     }
 }
